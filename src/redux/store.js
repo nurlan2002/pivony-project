ac4ee79245da4fe33ad70537c16466e48b6cea27
nurlan2002 <nurlandadashov2002@yahoo.com>
2021-04-10 15:30:14 +0400

    removed redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
-
-import { createLogger } from "redux-logger";
-
 import thunkMiddleware from "redux-thunk";
 import { saveState } from "./loadState";
 
 import rootReducer from "./rootReducer";
 
-const logger = createLogger();
-
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 store.subscribe(() => {
     saveState(store.getState())
